430a55b11f6747fcf84bbaec7b7417d995cd8274
Loïc Dumortier <loic.dumortier@telenet.be>
2021-09-27 21:21:19 +0200

    working update

---
+++ ralos/animation.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.animation import FuncAnimation, FFMpegWriter
+
+from celestialobject import CelestialObject
+from solarsystem import SolarSystem
+
+solarsystem = SolarSystem(1.989e30, 84600)
+solarsystem.add_celestial(mass=5.972e24,
+                          location=np.array([150.46e9, 0]),
+                          velocity=np.array([0, 29785.0]),
+                          name='earth')
+fig, ax = plt.subplots()
+ax.set_facecolor('black')
+ax.set_xlim(-180e9, 180e9)
+ax.set_ylim(-180e9, 180e9)
+
+
+def animate(i):
+    solarsystem.update(timestep=86400)
+    ax.plot(solarsystem.celestials[0].location[0], 
+            solarsystem.celestials[0].location[1], 
+            'yo')
+    ax.plot(solarsystem.celestials[1].location[0], 
+            solarsystem.celestials[1].location[1], 
+            'bo')
+
+ani = FuncAnimation(fig, animate, frames=365)
+writermp4=FFMpegWriter(fps=30)
+ani.save('solar.mp4', writer=writermp4)
--- ralos/mainscript.py
+++ ralos/mainscript.py
@@ -1,24 +1,27 @@
-from celestialobject import CelestialObject
-from solarsystem import SolarSystem
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.animation import FuncAnimation
+
+from celestialobject import CelestialObject
+from solarsystem import SolarSystem
 
 solarsystem = SolarSystem(1.989e30, 84600)
 solarsystem.add_celestial(mass=5.972e24,
                           location=np.array([150.46e9, 0]),
                           velocity=np.array([0, 29785.0]),
                           name='earth')
-
-print(solarsystem.celestials)
 sun = []
 earth = []
-for i in range(180):
+
+for i in range(1000):
     solarsystem.update(timestep=86400)
     sun.append(solarsystem.celestials[0].location)
     earth.append(solarsystem.celestials[1].location)
 sun = np.array(sun)
 earth = np.array(earth)
 
-plt.scatter(earth[:, 0], earth[:, 1], c='blue')
-plt.scatter(sun[:, 0], sun[:, 1], c='yellow')
+fig, ax = plt.subplots()
+ax.set_facecolor('black')
+ax.scatter(earth[:, 0], earth[:, 1], c='blue', s=10)
+ax.scatter(sun[:, 0], sun[:, 1], c='yellow', s=100)
 plt.show()
